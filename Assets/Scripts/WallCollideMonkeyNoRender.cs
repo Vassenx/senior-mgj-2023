0d97278d4e8f226f58714f3b842999cf445696ab
mr-fufu <monkeymanp8@gmail.com>
2023-01-21 20:48:37 -0500

    Update wall occlusion with new collider to hide walls and update some wall colliders to hide faster

--- Assets/Scripts/WallCollideMonkeyNoRender.cs
+++ Assets/Scripts/WallCollideMonkeyNoRender.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class WallCollideMonkeyNoRender : MonoBehaviour
 {
     [SerializeField] private Transform cameraTransform;
-    
+    private Vector3 cameraDirection;
+    public float cameraDistance;
+
     private void Update()
     {
-        transform.position = cameraTransform.position; // hack: wacky Cinemachine issues
+        cameraDirection = (cameraTransform.position - transform.parent.position);
+        cameraDirection.y = 0;
+        cameraDirection = (cameraDirection.normalized * cameraDistance) + transform.parent.position;
+        transform.position = new Vector3(cameraDirection.x, 1, cameraDirection.z); // hack: wacky Cinemachine issues
+        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, cameraTransform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
     }
 
     private void OnTriggerEnter(Collider other)
